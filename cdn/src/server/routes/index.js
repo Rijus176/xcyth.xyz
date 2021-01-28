7af02f2b2ff5a97aa1ea39c2851cb498277da0dc
RitamC6 <65271902+RitamC6@users.noreply.github.com>
2021-01-28 21:48:39 +0530

    remove sensitive stuff

--- cdn/src/server/routes/index.js
+++
@@ -1,7 +0,0 @@
-require('fs')
-    .readdirSync(__dirname)
-    .map(filename => {
-        const moduleName = filename.split('.')[0];
-        // eslint-disable-next-line global-require
-        exports[moduleName] = require(`${__dirname}/${filename}`);
-    });
